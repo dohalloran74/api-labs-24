a89f6130ca09079ef52ea2d9a2e38d3ad736402d
deirdreOH <dohalloran@wit.ie>
2024-12-06 15:56:31 +0000

    Mongo Lab Exercises Done

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,10 +3,15 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+// other imports
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+// Enable CORS for all requests
+app.use(cors());
 
 const port = process.env.PORT;
 

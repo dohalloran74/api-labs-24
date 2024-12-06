f2052349d162a54e71e97a60f89991f8277e6f86
deirdreOH <dohalloran@wit.ie>
2024-12-06 15:31:08 +0000

    Mongo lab users router

--- tasky-api/eslint.config.mjs
+++ tasky-api/eslint.config.mjs
@@ -6,9 +6,10 @@ export default [{
         files: ["**/*.js"],
         languageOptions: {
             globals: globals.node,
-            ecmaVersion: 2015, // ES6
+            ecmaVersion: 2021, // ES6
             sourceType: "module", // Use module format
         },
+
         rules: {
             "semi": 1,
             "no-console": "off",

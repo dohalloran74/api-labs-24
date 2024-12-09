7bc67ac313fe45546ff0190b907710768c8f6d3c
deirdreOH <dohalloran@wit.ie>
2024-12-09 18:08:28 +0000

    upcoming movies from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,9 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
+import {
+  getUpcomingMovies
+} from '../tmdb-api';
 
 const router = express.Router();
 
@@ -20,4 +23,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+  const upcomingMovies = await getUpcomingMovies();
+  res.status(200).json(upcomingMovies);
+}));
 export default router;
